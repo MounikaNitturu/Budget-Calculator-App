babb97a64ad88e8272161bc32812284a58eb9e77
Mounika Nitturu <108521373+MounikaNitturu@users.noreply.github.com>
2023-05-26 19:39:21 +0530

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,34 +11,25 @@ export class MainComponent {
   moneyEarned:number=0;
   moneyAvailable:number=0;
   moneySpent:number=0;
-  // incomeArray:BudgetItem[]=[];
-  // expenseArray:BudgetItem[]=[];
   constructor(private service:IncomeExpensesService){
-
   }
 
   onaddincome(event:any){
-    // console.log(event)
     this.moneyEarned+=event.amount
     this.moneyAvailable+=event.amount
     this.service.incomeArray.push(event)
-    // console.log(this.service.incomeArray)
   }
   onaddexpense(event:any){
-    // console.log(event)
     this.moneyAvailable-=event.amount;
     this.moneySpent+=event.amount;
     this.service.expenseArray.push(event)
-    // console.log(this.service.expenseArray)
   }
   incomeItemDeleted(incomeremoved:any){
     this.moneyEarned-=incomeremoved
     this.moneyAvailable-=incomeremoved
-    console.log(`Income delted ${incomeremoved}`)
   }
   expenseItemDeleted(expenseremoved:any){
     this.moneySpent-=expenseremoved
     this.moneyAvailable+=expenseremoved
-    console.log(`Expense delted ${expenseremoved}`)
   }
 }
